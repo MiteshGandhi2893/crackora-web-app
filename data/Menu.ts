c3f867759adc0f9002a285c96a8ce8ca53cc4c53
unknown <miteshgandhi28@gmail.com>
2026-02-08 19:22:25 +0530

    fixed build errors

--- data/Menu.ts
+++ data/Menu.ts
@@ -9,7 +9,7 @@ import {
 
 export const menu: Menu[] = [
   {
-    id: 1,
+    id: "1",
     label: "Mock Tests",
     href: "/mock-tests",
     icon: BiWindowAlt,
@@ -23,14 +23,14 @@ export const menu: Menu[] = [
     isActive: false,
   },
   {
-    id: 3,
+    id: "3",
     label: "Past Papers",
     href: "/past-papers",
     icon: BiSolidFileBlank,
     isActive: false,
   },
   {
-    id: 4,
+    id: "4",
     label: "Dashboard",
     href: "/dashboard",
     icon: BiSolidDashboard,
